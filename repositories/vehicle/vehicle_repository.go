6901bfabd35ee2e207a16451f03fad7b10b93893
Debkanchan Samadder <debu.samadder@gmail.com>
2023-05-23 13:31:14 +0530

    feat: add support for multiple vehicle type

--- repositories/vehicle/vehicle_repository.go
+++ repositories/vehicle/vehicle_repository.go
@@ -4,12 +4,15 @@ package vehiclerepository
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"strings"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
+	"github.com/bufbuild/connect-go"
 	pb "github.com/ride-app/driver-service/api/gen/ride/driver/v1alpha1"
 )
 
@@ -40,21 +43,41 @@ func (r *FirebaseImpl) GetVehicle(ctx context.Context, id string) (*pb.Vehicle, 
 	if err != nil {
 		return nil, err
 	}
+
+	var vehicleType pb.Vehicle_Type
+
+	switch doc.Data()["type"] {
+	case strings.ToLower(strings.Split(pb.Vehicle_TYPE_AUTORICKSHAW.String(), "_")[1]):
+		vehicleType = pb.Vehicle_TYPE_AUTORICKSHAW
+	case strings.ToLower(strings.Split(pb.Vehicle_TYPE_ERICKSHAW.String(), "_")[1]):
+		vehicleType = pb.Vehicle_TYPE_ERICKSHAW
+	case strings.ToLower(strings.Split(pb.Vehicle_TYPE_MOTORCYCLE.String(), "_")[1]):
+		vehicleType = pb.Vehicle_TYPE_MOTORCYCLE
+	default:
+		vehicleType = pb.Vehicle_TYPE_UNSPECIFIED
+	}
+
+	if vehicleType == pb.Vehicle_TYPE_UNSPECIFIED {
+		return nil, connect.NewError(connect.CodeFailedPrecondition, errors.New("unknown vehicle type"))
+	}
 	// Hardcode e-rickshaw for now
 	vehicle := pb.Vehicle{
 		Name:         "drivers/" + id + "/vehicle",
-		Type:         pb.Vehicle_TYPE_ERICKSHAW,
+		Type:         vehicleType,
 		DisplayName:  "E-Rickshaw",
 		CreateTime:   timestamppb.New(doc.CreateTime),
 		UpdateTime:   timestamppb.New(doc.UpdateTime),
-		LicensePlate: "",
+		LicensePlate: fmt.Sprintf("%v", doc.Data()["license_plate"]),
 	}
 
 	return &vehicle, nil
 }
 
 func (r *FirebaseImpl) UpdateVehicle(ctx context.Context, vehicle *pb.Vehicle) (updateTime *timestamppb.Timestamp, err error) {
-	x, err := r.firestore.Collection("vehicles").Doc(strings.Split(vehicle.Name, "/")[1]).Set(ctx, map[string]interface{}{})
+	x, err := r.firestore.Collection("vehicles").Doc(strings.Split(vehicle.Name, "/")[1]).Set(ctx, map[string]interface{}{
+		"license_plate": vehicle.LicensePlate,
+		"type":          strings.ToLower(strings.Split(vehicle.Type.String(), "_")[1]),
+	})
 
 	if err != nil {
 		return nil, err
