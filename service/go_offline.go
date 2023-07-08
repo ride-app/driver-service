0d624e98f5e9003dbffd3f7ee04acbeca9710130
Debkanchan Samadder <debu.samadder@gmail.com>
2023-07-08 17:07:59 +0530

    refactor: use connect errors instead of grpc errors

--- service/go_offline.go
+++ service/go_offline.go
@@ -6,14 +6,12 @@ import (
 
 	"github.com/bufbuild/connect-go"
 	pb "github.com/ride-app/driver-service/api/gen/ride/driver/v1alpha1"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func (service *DriverServiceServer) GoOffline(ctx context.Context,
 	req *connect.Request[pb.GoOfflineRequest]) (*connect.Response[pb.GoOfflineResponse], error) {
 	if err := req.Msg.Validate(); err != nil {
-		return nil, status.Error(codes.InvalidArgument, err.Error())
+		return nil, connect.NewError(connect.CodeInvalidArgument, err)
 	}
 
 	driverId := strings.Split(req.Msg.Name, "/")[1]
