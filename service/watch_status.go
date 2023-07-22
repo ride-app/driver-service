ff4d671b933d6c6c4ed14ccb69a6ad6ef9571c11
Debkanchan Samadder <debu.samadder@gmail.com>
2023-07-23 00:26:28 +0530

    created initial handler

---
+++ service/watch_status.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/bufbuild/connect-go"
+	pb "github.com/ride-app/driver-service/api/gen/ride/driver/v1alpha1"
+)
+
+func (service *DriverServiceServer) WatchStatus(ctx context.Context,
+	req *connect.Request[pb.WatchStatusRequest]) (*connect.ServerStream[pb.WatchStatusResponse], error) {
+
+	if err := req.Msg.Validate(); err != nil {
+		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+	}
+
+	driverId := strings.Split(req.Msg.Name, "/")[1]
+
+	if driverId != req.Header().Get("Authorization") {
+		return nil, connect.NewError(connect.CodePermissionDenied, errors.New("permission denied"))
+	}
+
+	status, err := service.driverRepository.GetStatus(ctx, driverId)
+
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+
+	if status == nil {
+		return nil, connect.NewError(connect.CodeNotFound, errors.New("status not found"))
+	}
+
+	return connect.NewServerStreamHandler(&pb.GetStatusResponse{
+		Status: status,
+	}), nil
+}
