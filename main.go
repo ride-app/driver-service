b73fb7e6adcc8fb3662a1acc3d9f1c6869b67be2
Debkanchan Samadder <debu.samadder@gmail.com>
2023-08-02 11:46:38 +0530

    refactor main

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/bufbuild/connect-go"
 	"github.com/ilyakaznacheev/cleanenv"
@@ -12,19 +11,25 @@ import (
 	"github.com/ride-app/driver-service/config"
 	"github.com/ride-app/driver-service/di"
 	"github.com/ride-app/driver-service/interceptors"
-	"github.com/sirupsen/logrus"
+	"github.com/ride-app/driver-service/logger"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
 func main() {
+	log := logger.New()
+
+	err := cleanenv.ReadEnv(&config.Env)
+
+	log.WithError(err).Fatal("Failed to read environment variables")
+
 	service, err := di.InitializeService()
 
 	if err != nil {
-		logrus.Fatalf("Failed to initialize service: %v", err)
+		log.Fatalf("Failed to initialize service: %v", err)
 	}
 
-	logrus.Info("Service Initialized")
+	log.Info("Service Initialized")
 
 	// Create a context that, when cancelled, ends the JWKS background refresh goroutine.
 	ctx, cancel := context.WithCancel(context.Background())
@@ -34,7 +39,7 @@ func main() {
 	authInterceptor, err := interceptors.NewAuthInterceptor(ctx)
 
 	if err != nil {
-		logrus.Fatalf("Failed to initialize auth interceptor: %v", err)
+		log.Fatalf("Failed to initialize auth interceptor: %v", err)
 	}
 
 	connectInterceptors := connect.WithInterceptors(authInterceptor)
@@ -49,29 +54,4 @@ func main() {
 		h2c.NewHandler(mux, &http2.Server{}),
 	))
 
-}
-
-func init() {
-	logrus.SetReportCaller(true)
-
-	logrus.SetFormatter(&logrus.JSONFormatter{
-		FieldMap: logrus.FieldMap{
-			logrus.FieldKeyTime:  "timestamp",
-			logrus.FieldKeyLevel: "severity",
-			logrus.FieldKeyMsg:   "message",
-		},
-		TimestampFormat: time.RFC3339Nano,
-	})
-
-	logrus.SetLevel(logrus.InfoLevel)
-
-	err := cleanenv.ReadEnv(&config.Env)
-
-	if config.Env.Debug {
-		logrus.SetLevel(logrus.DebugLevel)
-	}
-
-	if err != nil {
-		logrus.Warnf("Could not load config: %v", err)
-	}
 }
