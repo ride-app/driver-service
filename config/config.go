5776616755c413cfb85bdb23c7b8feff280a7f6c
Debkanchan Samadder <debu.samadder@gmail.com>
2023-08-11 21:36:42 +0530

    ci: rename debug to log debug

--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 type ConfigStruct struct {
 	Production        bool   `env:"PRODUCTION" env-description:"dev or prod" env-default:"true"`
-	Debug             bool   `env:"DEBUG" env-description:"should log at debug level" env-default:"false"`
+	LogDebug          bool   `env:"LOG_DEBUG" env-description:"should log at debug level" env-default:"false"`
 	Port              int32  `env:"PORT" env-description:"server port" env-default:"50051"`
 	ProjectID         string `env:"PROJECT_ID" env-description:"project id" env-default:"NO_PROJECT"`
 	WalletServiceHost string `env:"WALLET_SERVICE_HOST" env-description:"wallet service host" env-default:"localhost:50052"`
