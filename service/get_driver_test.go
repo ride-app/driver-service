66affd17ed2b61502cf004e693eb1cba3019ccf2
Debkanchan Samadder <debu.samadder@gmail.com>
2023-08-14 15:23:44 +0530

    test: add get driver tests

--- service/get_driver_test.go
+++ service/get_driver_test.go
@@ -3,54 +3,207 @@
 
 package service_test
 
-// var _ = Describe("GetDriver", func() {
-// 	var (
-// 		ctrl            *gomock.Controller
-// 		mockDriverRepo  *mocks.MockDriverRepository
-// 		mockVehicleRepo *mocks.MockVehicleRepository
-// 		mockWalletRepo  *mocks.MockWalletRepository
-// 		mockLogger      *mocks.MockLogger
-// 		service         *driverService.DriverServiceServer
-// 	)
+import (
+	"context"
+	"errors"
 
-// 	BeforeEach(func() {
-// 		ctrl = gomock.NewController(GinkgoT())
-// 		mockDriverRepo = mocks.NewMockDriverRepository(ctrl)
-// 		mockVehicleRepo = mocks.NewMockVehicleRepository(ctrl)
-// 		mockWalletRepo = mocks.NewMockWalletRepository(ctrl)
-// 		mockLogger = &mocks.MockLogger{}
-// 		service = driverService.New(mockDriverRepo, mockVehicleRepo, mockWalletRepo, mockLogger)
-// 	})
+	"github.com/bufbuild/connect-go"
+	"github.com/golang/protobuf/proto"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	pb "github.com/ride-app/driver-service/api/gen/ride/driver/v1alpha1"
+	"github.com/ride-app/driver-service/mocks"
+	driverService "github.com/ride-app/driver-service/service"
+	"go.uber.org/mock/gomock"
+	"google.golang.org/genproto/googleapis/type/date"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
 
-// 	AfterEach(func() {
-// 		ctrl.Finish()
-// 	})
+var _ = Describe("GetDriver", func() {
+	var (
+		ctrl            *gomock.Controller
+		mockDriverRepo  *mocks.MockDriverRepository
+		mockVehicleRepo *mocks.MockVehicleRepository
+		mockWalletRepo  *mocks.MockWalletRepository
+		mockLogger      *mocks.MockLogger
+		service         *driverService.DriverServiceServer
+	)
 
-// 	Context("when the request is valid", func() {
-// 		It("returns the driver", func() {
-// 			req := &connect.Request[pb.GetDriverRequest]{
-// 				Msg: &pb.GetDriverRequest{
-// 					Name: "drivers/1",
-// 				},
-// 			}
-// 			mockDriverRepo.EXPECT().GetDriver(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, nil)
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		mockDriverRepo = mocks.NewMockDriverRepository(ctrl)
+		mockVehicleRepo = mocks.NewMockVehicleRepository(ctrl)
+		mockWalletRepo = mocks.NewMockWalletRepository(ctrl)
+		mockLogger = &mocks.MockLogger{}
+		service = driverService.New(mockDriverRepo, mockVehicleRepo, mockWalletRepo, mockLogger)
+	})
 
-// 			_, err := service.GetDriver(context.Background(), req)
-// 			Expect(err).To(BeNil())
-// 		})
-// 	})
+	JustBeforeEach(func() {
+		mockDriverRepo.EXPECT().GetDriver(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, nil).AnyTimes()
+		mockDriverRepo.EXPECT().GetDriver(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, nil).AnyTimes()
+		mockDriverRepo.EXPECT().UpdateDriver(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, nil).AnyTimes()
+		mockDriverRepo.EXPECT().DeleteDriver(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, nil).AnyTimes()
+		mockVehicleRepo.EXPECT().GetVehicle(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, nil).AnyTimes()
+		mockWalletRepo.EXPECT().GetWallet(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, nil).AnyTimes()
+	})
 
-// 	Context("when the request is invalid", func() {
-// 		It("returns an error", func() {
-// 			req := &connect.Request[pb.GetDriverRequest]{
-// 				Msg: &pb.GetDriverRequest{
-// 					Name: "",
-// 				},
-// 			}
-// 			mockDriverRepo.EXPECT().GetDriver(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, errors.New("invalid request"))
+	AfterEach(func() {
+		ctrl.Finish()
+	})
 
-// 			_, err := service.GetDriver(context.Background(), req)
-// 			Expect(err).To(MatchError("invalid request"))
-// 		})
-// 	})
-// })
+	Context("given the request is valid", func() {
+		var (
+			req *connect.Request[pb.GetDriverRequest]
+		)
+
+		BeforeEach(func() {
+			req = &connect.Request[pb.GetDriverRequest]{
+				Msg: &pb.GetDriverRequest{
+					Name: "drivers/valid-driver-id",
+				},
+			}
+
+			req.Header().Set("uid", "valid-driver-id")
+		})
+
+		When("the driver does not exist", func() {
+			BeforeEach(func() {
+				mockDriverRepo.EXPECT().GetDriver(gomock.Any(), gomock.Any(), gomock.Eq("valid-driver-id")).Return(nil, nil)
+			})
+			It("returns not found error", func() {
+				_, err := service.GetDriver(context.Background(), req)
+				Expect(err).To(SatisfyAll(
+					HaveOccurred(),
+					BeAssignableToTypeOf(&connect.Error{}),
+					WithTransform(func(err error) connect.Code {
+						return err.(*connect.Error).Code()
+					}, Equal(connect.CodeNotFound)),
+				))
+			})
+		})
+
+		When("the driver does exist", func() {
+			var driver *pb.Driver
+
+			BeforeEach(func() {
+				driver = &pb.Driver{
+					Name:        "drivers/valid-driver-id",
+					DisplayName: "John Doe",
+					PhotoUri:    "https://example.com/photo.jpg",
+					PhoneNumber: "+911234567890",
+					DateOfBirth: &date.Date{
+						Year:  1990,
+						Month: 1,
+						Day:   1,
+					},
+					Gender:     pb.Driver_GENDER_MALE,
+					CreateTime: timestamppb.Now(),
+					UpdateTime: timestamppb.Now(),
+				}
+
+				mockDriverRepo.EXPECT().GetDriver(gomock.Any(), gomock.Any(), gomock.Eq("valid-driver-id")).Return(driver, nil)
+			})
+
+			AfterEach(func() {
+				driver = nil
+			})
+
+			It("returns the driver", func() {
+				res, err := service.GetDriver(context.Background(), req)
+				Expect(err).To(BeNil())
+				Expect(proto.Equal(driver, res.Msg.Driver)).To(BeTrue())
+			})
+
+		})
+
+		When("driver repository GetDriver returns error", func() {
+			BeforeEach(func() {
+				mockDriverRepo.EXPECT().GetDriver(gomock.Any(), gomock.Any(), gomock.Eq("valid-driver-id")).Return(nil, errors.New("error"))
+			})
+
+			It("returns internal error", func() {
+				_, err := service.GetDriver(context.Background(), req)
+				Expect(err).To(SatisfyAll(
+					HaveOccurred(),
+					BeAssignableToTypeOf(&connect.Error{}),
+					WithTransform(func(err error) connect.Code {
+						return err.(*connect.Error).Code()
+					}, Equal(connect.CodeInternal)),
+				))
+			})
+		})
+	})
+
+	Context("given all other request parameters are valid", func() {
+		var req *connect.Request[pb.GetDriverRequest]
+
+		BeforeEach(func() {
+			req = &connect.Request[pb.GetDriverRequest]{
+				Msg: &pb.GetDriverRequest{
+					Name: "drivers/valid-driver-id",
+				},
+			}
+			req.Header().Set("uid", "valid-driver-id")
+		})
+
+		When("driver name is empty", func() {
+			It("returns invalid argument error", func() {
+				req.Msg.Name = ""
+
+				_, err := service.GetDriver(context.Background(), req)
+				Expect(err).To(SatisfyAll(
+					HaveOccurred(),
+					BeAssignableToTypeOf(&connect.Error{}),
+					WithTransform(func(err error) connect.Code {
+						return err.(*connect.Error).Code()
+					}, Equal(connect.CodeInvalidArgument)),
+				))
+			})
+		})
+
+		When("driver name does not match drivers/driverId pattern", func() {
+			It("returns invalid argument error", func() {
+				req.Msg.Name = "not-drivers/invalid-driver-id"
+
+				_, err := service.GetDriver(context.Background(), req)
+				Expect(err).To(SatisfyAll(
+					HaveOccurred(),
+					BeAssignableToTypeOf(&connect.Error{}),
+					WithTransform(func(err error) connect.Code {
+						return err.(*connect.Error).Code()
+					}, Equal(connect.CodeInvalidArgument)),
+				))
+			})
+		})
+
+		When("uid header is missing in request", func() {
+			It("returns permission denied error", func() {
+				req.Header().Del("uid")
+
+				_, err := service.GetDriver(context.Background(), req)
+				Expect(err).To(SatisfyAll(
+					HaveOccurred(),
+					BeAssignableToTypeOf(&connect.Error{}),
+					WithTransform(func(err error) connect.Code {
+						return err.(*connect.Error).Code()
+					}, Equal(connect.CodePermissionDenied)),
+				))
+			})
+		})
+
+		When("uid header is not equal to driver id", func() {
+			It("returns permission denied error", func() {
+				req.Header().Set("uid", "invalid-driver-id")
+
+				_, err := service.GetDriver(context.Background(), req)
+				Expect(err).To(SatisfyAll(
+					HaveOccurred(),
+					BeAssignableToTypeOf(&connect.Error{}),
+					WithTransform(func(err error) connect.Code {
+						return err.(*connect.Error).Code()
+					}, Equal(connect.CodePermissionDenied)),
+				))
+			})
+		})
+	})
+})
